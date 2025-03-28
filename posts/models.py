92b9e31235b4c1c7e553e4b59c1b1d8b8e32caef
juancho391 <juanchischis2010@hotmail.com>
2025-03-28 11:54:54 -0500

    Creando los modelos en users y en posts

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,22 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from users.models import User
+
+class Posts(models.Model):
+  post_img = models.CharField(max_length=100,null=True)
+  likes = models.IntegerField(default=0)
+  description = models.CharField(max_length=500, null=True)
+  publish_date = models.DateTimeField(auto_now_add=True)
+  id_user = models.ForeignKey(User, on_delete=models.CASCADE)
+  
+
+
+class Likes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_post = models.ForeignKey(Posts, on_delete=models.CASCADE, related_name='post')
+    
+
+class Comments(models.Model):
+   description = models.TextField(max_length=100)
+   id_post = models.ForeignKey(Posts, on_delete=models.CASCADE, related_name='comments')
+   id_user = models.ForeignKey(User, on_delete=models.CASCADE)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
+# Create your models here.
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    description  = models.TextField(null=True, blank=True)
+    profile_img = models.CharField(null=True, blank=True)
+    birth_date = models.DateTimeField(null=False)
